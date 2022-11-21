287f77d393dd97a22bcbae0675a75ba367bd36c3
Matias Saggiorato <matiassaggiorato@gmail.com>
2022-11-21 15:16:52 -0300

    Remove debug information

--- do-push.sh
+++ do-push.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo "$SOURCE_DIR"
 if [[ $SOURCE_DIR != /* ]]; then
 	SOURCE_DIR="${GITHUB_WORKSPACE}/${SOURCE_DIR}"
 fi
-echo "$SOURCE_DIR"
 
 cd "${SOURCE_DIR}" || exit 1;
 echo "::group::Pushing"
